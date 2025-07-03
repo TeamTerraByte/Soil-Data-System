5d7d9b5aa039fa25e1af230dd247ac75f13ec3e4
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2025-07-03 15:59:37 -0500

    720 Error

--- Maduino-HTTP-test/Maduino-HTTP-test.ino
+++ Maduino-HTTP-test/Maduino-HTTP-test.ino
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <SPI.h>
+#include "secrets.h"
 
-#define SOIL_SENSOR_PIN A1
 #define DEBUG true
 
 // LTE Control Pins
@@ -10,14 +10,15 @@
 #define LTE_PWRKEY_PIN 5
 #define LTE_FLIGHT_PIN 7
 
-#define SD_CS_PIN 4  // Adjust if different
+// #define SD_CS_PIN 4  // Adjust if different
 
-String Apikey = ""; // ThingSpeak API Key
+String Apikey = API_WRITE_KEY; // ThingSpeak API Key
 File logFile;
 
 void setup() {
   SerialUSB.begin(115200);
   Serial1.begin(115200);
+  
 
   // LTE module power sequence
   pinMode(LTE_RESET_PIN, OUTPUT);
@@ -32,7 +33,7 @@ void setup() {
   pinMode(LTE_FLIGHT_PIN, OUTPUT);
   digitalWrite(LTE_FLIGHT_PIN, LOW); // Normal mode
 
-  delay(5000); // Wait for LTE module
+  delay(30000); // Wait for LTE module
 
   // LTE network setup
   sendData("AT+CCID", 3000, DEBUG);
@@ -40,55 +41,56 @@ void setup() {
   sendData("AT+CGATT=1", 1000, DEBUG);
   sendData("AT+CGACT=1,1", 1000, DEBUG);
   sendData("AT+CGDCONT=1,\"IP\",\"fast.t-mobile.com\"", 1000, DEBUG);
+  sendData("AT+CGPADDR=1", 3000, DEBUG);          // show pdp address
 
   // SD card initialization
-  if (!SD.begin(SD_CS_PIN)) {
-    SerialUSB.println("SD card initialization failed!");
-  } else {
-    SerialUSB.println("SD card ready.");
-    logFile = SD.open("SOILMOIS.CSV", FILE_WRITE);
-    if (logFile) {
-      logFile.println("Timestamp(s),SoilMoisture(%)"); // Header
-      logFile.close();
-    }
-  }
+  // if (!SD.begin(SD_CS_PIN)) {
+  //   SerialUSB.println("SD card initialization failed!");
+  // } else {
+  //   SerialUSB.println("SD card ready.");
+  //   logFile = SD.open("SOILMOIS.CSV", FILE_WRITE);
+  //   if (logFile) {
+  //     logFile.println("Timestamp(s),SoilMoisture(%)"); // Header
+  //     logFile.close();
+  //   }
+  // }
 
   SerialUSB.println("Soil Sensor 4G LTE Ready!");
 }
 
 void loop() {
-  int soilRaw = analogRead(SOIL_SENSOR_PIN);
-  float soilPercent = (float)(1023 - soilRaw) * 100.0 / 1023.0;
-  unsigned long timestamp = millis() / 1000; // seconds since boot
+  // int soilRaw = analogRead(SOIL_SENSOR_PIN);
+  // float soilPercent = (float)(1023 - soilRaw) * 100.0 / 1023.0;
+  // unsigned long timestamp = millis() / 1000; // seconds since boot
 
-  // Log to Serial
-  SerialUSB.print("Time: ");
-  SerialUSB.print(timestamp);
-  SerialUSB.print("s | Soil Moisture: ");
-  SerialUSB.print(soilPercent);
-  SerialUSB.println("%");
+  // // Log to Serial
+  // SerialUSB.print("Time: ");
+  // SerialUSB.print(timestamp);
+  // SerialUSB.print("s | Soil Moisture: ");
+  // SerialUSB.print(soilPercent);
+  // SerialUSB.println("%");
 
-  // Save to SD card
-  logFile = SD.open("SOILMOIS.CSV", FILE_WRITE);
-  if (logFile) {
-    logFile.print(timestamp);
-    logFile.print(",");
-    logFile.println(soilPercent);
-    logFile.close();
-    SerialUSB.println("Data saved to SD card.");
-  } else {
-    SerialUSB.println("Failed to write to SD card.");
-  }
+  // // Save to SD card
+  // logFile = SD.open("SOILMOIS.CSV", FILE_WRITE);
+  // if (logFile) {
+  //   logFile.print(timestamp);
+  //   logFile.print(",");
+  //   logFile.println(soilPercent);
+  //   logFile.close();
+  //   SerialUSB.println("Data saved to SD card.");
+  // } else {
+  //   SerialUSB.println("Failed to write to SD card.");
+  // }
 
   // Send to ThingSpeak
   sendData("AT+HTTPINIT", 2000, DEBUG);
-  String http_str = "AT+HTTPPARA=\"URL\",\"http://api.thingspeak.com/update?api_key=" + Apikey + "&field1=" + String((int)soilPercent) + "\"";
+  String http_str = "AT+HTTPPARA=\"URL\",\"http://api.thingspeak.com/update?api_key=" + Apikey + "&field1=" + String((int)67) + "\"";
   sendData(http_str, 2000, DEBUG);
   sendData("AT+HTTPPARA=\"CONTENT\",\"application/x-www-form-urlencoded\"", 1000, DEBUG);
-  sendData("AT+HTTPACTION=0", 5000, DEBUG);
+  sendData("AT+HTTPACTION=0", 30000, DEBUG);
   sendData("AT+HTTPTERM", 3000, DEBUG);
 
-  delay(60000); // 2 minutes
+  delay(60000);
 }
 
 String sendData(String command, const int timeout, boolean debug) {
