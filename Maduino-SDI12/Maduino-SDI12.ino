f872eed0a622596deaa69aeeec20d73a11130bac
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2025-07-22 16:38:01 -0500

    Some progress on Maduino SDI12

--- Maduino-SDI12/Maduino-SDI12.ino
+++ Maduino-SDI12/Maduino-SDI12.ino
@@ -1,291 +1,49 @@
 #include <SDI12.h>
-#include <Wire.h>
 
-#define DATA_PIN 2        // Blue wire connected to digital pin 2
-// #define POWER_PIN 3       // Controls relay power to sensor
-#define SLAVE_ADDRESS 0x08  // I2C address of the Maduino Zero
-
-SDI12 mySDI12(DATA_PIN);
-
-String probeAddress = "C";  // Default probe address
-unsigned long lastMeasurement = 0;
-const unsigned long MEASUREMENT_INTERVAL = 180000; // 3 minutes
-const unsigned long POWER_STABILIZATION_DELAY = 5000; // 5 seconds for voltage stabilization
-// bool sensorPowered = false;
+#define DATA_PIN 2
+SDI12 sdi12(DATA_PIN);
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(); // Initialize I2C as master
-  
-  // Small delay to let everything initialize
-  delay(POWER_STABILIZATION_DELAY);
-  
-  // Initialize SDI-12 after sensor is powered and stabilized
-  mySDI12.begin();
-  
-  // Initialize and find probe address
-  while(!initializeProbe()) {
-    delay(1000);
-  }
-  
-  // Take first measurement immediately
-  takeMeasurements();
-  lastMeasurement = millis();
+  SerialUSB.begin(115200);
+  while(!SerialUSB); // wait
+  SerialUSB.println("=================== NEW TEST ===================");
+  sdi12.begin();
 }
 
 void loop() {
-  // Check if it's time for next measurement
-  if (millis() - lastMeasurement >= MEASUREMENT_INTERVAL) {
+  resetSDI();
+  queryDevice("?!", 2000);
+  resetSDI();
+  queryDevice("CI!", 2000);
+  delay(5000);
+}
 
-    // Re-initialize SDI-12 communication
-    mySDI12.begin();
-    
-    // Take measurements
-    takeMeasurements();
-    lastMeasurement = millis();
-  }
-  
-  // Check for manual commands from Serial Monitor
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    command.trim();
-    if (command.length() > 0) {
-      
-      sendCommand(command);
-    }
-  }
-
-  delay(100); // Small delay to prevent overwhelming the system
+void resetSDI(){
+  sdi12.end();
+  delay(50);     // Allow bus to settle, experiment with 20-100ms
+  sdi12.begin();
 }
 
 
-
-bool initializeProbe() {
-  // Query probe address
-  String response = sendCommand("?!");
-  if (response.length() > 0) {
-    probeAddress = response;
-    return true;
-  } else {
-    Serial.println("Soil Probe C not detected");
-    probeAddress = "C";
-    return false;
+String queryDevice(String cmd, int timeout) {
+  while (sdi12.available()){
+    sdi12.read();  // clear buffer
   }
+  sdi12.sendCommand(cmd);
   
-  delay(1000);
-}
-
-void takeMeasurements() {
-  // Measure soil moisture with salinity compensation
-  measureSoilMoisture();
-  
-  delay(500); // Small delay between measurements
-  
-  // Measure temperature in Celsius
-  measureTemperature();
-}
-
-void measureSoilMoisture() {
-  // Send measurement command (C0 = moisture with salinity compensation)
-  String measureCommand = probeAddress + "C0!";
-  String response = sendCommand(measureCommand);
-  
-  if (response.length() > 0) {
-    // Parse timing from response (format: TTTNNN where TTT is time, NNN is number of values)
-    // Wait for measurement to complete (add extra time for safety)
-    int measureTime = 3000; // Default 3 seconds if parsing fails
-    if (response.length() >= 6) {
-      String timeStr = response.substring(0, 3);
-      measureTime = timeStr.toInt() * 1000 + 1000; // Convert to ms and add 1s buffer
-    }
-    
-    delay(measureTime);
-    
-    // Request data
-    String dataCommand = probeAddress + "D0!";
-    String dataResponse = sendCommand(dataCommand);
-    
-    if (dataResponse.length() > 0) {
-      parseMoistureData(dataResponse);
-    }
-  }
-}
-
-void measureTemperature() {
-  // Send measurement command (C2 = temperature in Celsius)
-  String measureCommand = probeAddress + "C2!";
-  String response = sendCommand(measureCommand);
-  
-  if (response.length() > 0) {
-    // Wait for measurement to complete
-    int measureTime = 3000; // Default 3 seconds
-    if (response.length() >= 6) {
-      String timeStr = response.substring(0, 3);
-      measureTime = timeStr.toInt() * 1000 + 1000; // Convert to ms and add 1s buffer
-    }
-    
-    delay(measureTime);
-    
-    // Request data
-    String dataCommand = probeAddress + "D0!";
-    String dataResponse = sendCommand(dataCommand);
-    
-    if (dataResponse.length() > 0) {
-      parseTemperatureData(dataResponse);
-    }
-  }
-}
-
-void parseMoistureData(String data) {
-  // Data format: address + moisture values separated by + or -
-  String outputData = "Moist";
-  Serial.print(outputData);
-  
-  int startIndex = 1; // Skip the address character
-  
-  while (startIndex < data.length()) {
-    int nextPlus = data.indexOf('+', startIndex);
-    int nextMinus = data.indexOf('-', startIndex);
-    int nextDelim = -1;
-    
-    if (nextPlus == -1 && nextMinus == -1) break;
-    if (nextPlus == -1) nextDelim = nextMinus;
-    else if (nextMinus == -1) nextDelim = nextPlus;
-    else nextDelim = min(nextPlus, nextMinus);
-    
-    if (nextDelim > startIndex) {
-      String value = data.substring(startIndex, nextDelim);
-      outputData += ",";
-      outputData += value;
-      Serial.print(",");
-      Serial.print(value);
-    }
-    
-    startIndex = nextDelim + 1;
-    if (startIndex < data.length() && (data.charAt(startIndex-1) == '+' || data.charAt(startIndex-1) == '-')) {
-      // Find the end of this number
-      int endNum = startIndex;
-      while (endNum < data.length() && data.charAt(endNum) != '+' && data.charAt(endNum) != '-') {
-        endNum++;
-      }
-      String value = String(data.charAt(startIndex-1)) + data.substring(startIndex, endNum);
-      outputData += ",";
-      outputData += value;
-      Serial.print(",");
-      Serial.print(value);
-      startIndex = endNum;
-    }
-  }
-  Serial.println(); // End the CSV line
-  
-  // Transmit via I2C
-  transmitI2C(outputData);
-}
-
-void parseTemperatureData(String data) {
-  // Data format: address + temperature values separated by + or -
-  String outputData = "Temp";
-  Serial.print(outputData);
-  
-  int startIndex = 1; // Skip the address character
-  
-  while (startIndex < data.length()) {
-    int nextPlus = data.indexOf('+', startIndex);
-    int nextMinus = data.indexOf('-', startIndex);
-    int nextDelim = -1;
-    
-    if (nextPlus == -1 && nextMinus == -1) break;
-    if (nextPlus == -1) nextDelim = nextMinus;
-    else if (nextMinus == -1) nextDelim = nextPlus;
-    else nextDelim = min(nextPlus, nextMinus);
-    
-    if (nextDelim > startIndex) {
-      String value = data.substring(startIndex, nextDelim);
-      outputData += ",";
-      outputData += value;
-      Serial.print(",");
-      Serial.print(value);
-    }
-    
-    startIndex = nextDelim + 1;
-    if (startIndex < data.length() && (data.charAt(startIndex-1) == '+' || data.charAt(startIndex-1) == '-')) {
-      // Find the end of this number
-      int endNum = startIndex;
-      while (endNum < data.length() && data.charAt(endNum) != '+' && data.charAt(endNum) != '-') {
-        endNum++;
-      }
-      String value = String(data.charAt(startIndex-1)) + data.substring(startIndex, endNum);
-      outputData += ",";
-      outputData += value;
-      Serial.print(",");
-      Serial.print(value);
-      startIndex = endNum;
-    }
-  }
-  Serial.println(); // End the CSV line
-  
-  // Transmit via I2C
-  transmitI2C(outputData);
-}
-
-void transmitI2C(String data) {
-  // Split data into chunks if it's too long (I2C buffer limit is typically 32 bytes)
-  int maxChunkSize = 30; // Leave some buffer space
-  int dataLength = data.length();
-  
-  for (int i = 0; i < dataLength; i += maxChunkSize) {
-    int chunkEnd = min(i + maxChunkSize, dataLength);
-    String chunk = data.substring(i, chunkEnd);
-    
-    // Begin transmission to slave device
-    Wire.beginTransmission(SLAVE_ADDRESS);
-    delayMicroseconds(10);
-    
-    // Send chunk as bytes
-    Wire.write(chunk.c_str(), chunk.length());
-    
-    // If this is the last chunk, add newline character
-    if (chunkEnd >= dataLength) {
-      Wire.write('\n');
-    }
-    
-    // End transmission
-    byte error = Wire.endTransmission();
-    
-    // Check for transmission errors
-    if (error != 0) {
-      Serial.print("I2C Transmission error: ");
-      Serial.println(error);
-    }
-    
-    // Small delay between chunks if there are more
-    if (chunkEnd < dataLength) {
-      delay(10);
-    }
-  }
-}
-
-String sendCommand(String command) {
-  mySDI12.sendCommand(command);
-  
-  // Wait for response with timeout
   String response = "";
-  unsigned long startTime = millis();
-  const unsigned long timeout = 2000; // 2 second timeout
+  unsigned long start = millis();
   
-  while (millis() - startTime < timeout) {
-    if (mySDI12.available()) {
-      char c = mySDI12.read();
-      if (c == '\n' || c == '\r') {
-        if (response.length() > 0) {
-          return response;
-        }
-      } else {
-        response += c;
-      }
+  while (millis() - start < timeout) {
+    if (sdi12.available()) {
+      char c = sdi12.read();
+      if (c == '\n' || c == '\r') break;
+      response += c;
     }
-    delay(10);
   }
+  
+  SerialUSB.print("Command: " + cmd + " Response: ");
+  SerialUSB.println(response);
   
   return response;
 }
