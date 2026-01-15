b7cb4a52584c30f96f89b5e2b79f0e632dca67f6
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2026-01-15 11:18:21 -0600

    Working battery monitor

--- RootSense/Nano-Sketches/Nano-Battery-Monitor/Nano-Battery-Monitor.ino
+++ RootSense/Nano-Sketches/Nano-Battery-Monitor/Nano-Battery-Monitor.ino
@@ -1,18 +1,24 @@
 #define batteryPin A0
+// WARNING: This method only works for batteries producing less than 5V
+// Tutorial used for this code: https://www.youtube.com/watch?v=lec7kPv3VS8
+// For voltages > 5V, see this video: https://www.youtube.com/watch?v=hixEGmf1y5c 
+
+// Analog pins reads between 0 and 1023
+// 0 = 0V and 1023 = 5V
+
+const float mv_per_value = 4.882; 
 
 void setup(){
   Serial.begin(9600);
-  delay(200);
-  Serial.println("Serial init");
+  pinMode(batteryPin, INPUT);
 }
 
 void loop(){
-  static int i = 0;
-  static unsigned long start = millis();
-  if (millis() - start > 2000){
-    Serial.print("Hello ");
-    Serial.println(i);
-    i++;
-    start = millis();
-  }
+  int batteryReading = analogRead(batteryPin);
+  // convert reading -> ratio -> 4.77 V max 
+  float batteryVolts = batteryReading / 1023.0 * 4.77;
+  Serial.println("Battery reading " + String(batteryReading));
+  Serial.println("Battery voltage " + String(batteryVolts));
+  Serial.println();  // blank line
+  delay(5000);
 }
