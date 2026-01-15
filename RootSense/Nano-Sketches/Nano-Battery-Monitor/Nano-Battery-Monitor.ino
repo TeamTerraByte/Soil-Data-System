32deedf57a0796ee7ed509a1b769cbe875389bdb
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2026-01-15 10:41:47 -0600

    Nano hello world

---
+++ RootSense/Nano-Sketches/Nano-Battery-Monitor/Nano-Battery-Monitor.ino
@@ -0,0 +1,18 @@
+#define batteryPin A0
+
+void setup(){
+  Serial.begin(9600);
+  delay(200);
+  Serial.println("Serial init");
+}
+
+void loop(){
+  static int i = 0;
+  static unsigned long start = millis();
+  if (millis() - start > 2000){
+    Serial.print("Hello ");
+    Serial.println(i);
+    i++;
+    start = millis();
+  }
+}
