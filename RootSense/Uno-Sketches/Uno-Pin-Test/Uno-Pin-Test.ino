9a385b48233f8bb86c42ca5f0fae2920e999f4bd
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2026-01-25 15:58:57 -0600

    Update Uno-Pin-Test.ino

--- RootSense/Uno-Sketches/Uno-Pin-Test/Uno-Pin-Test.ino
+++ RootSense/Uno-Sketches/Uno-Pin-Test/Uno-Pin-Test.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#define TEST_PIN 9
 
+void setup() {
+  Serial.begin(9600);
+  delay(200);
+  pinMode(TEST_PIN, OUTPUT);
+  delay(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(TEST_PIN, HIGH);
+  delay(2000);
+  digitalWrite(TEST_PIN, LOW);
+  delay(2000);
 
 }
