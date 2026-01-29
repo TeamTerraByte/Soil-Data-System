b91c26a20c7029b7223ab3e800cbf435dd8e8580
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2026-01-29 08:49:31 -0600

    Parameterized filename

--- RootSense/Web-Interface/check_timestamp_gaps/check_timestamp_gaps.py
+++ RootSense/Web-Interface/check_timestamp_gaps/check_timestamp_gaps.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import csv
+import sys
 
 def parse_timestamp(ts: str) -> datetime:
     """
@@ -8,42 +9,65 @@ def parse_timestamp(ts: str) -> datetime:
     """
     return datetime.strptime(ts, "%m/%d/%Y, %H:%M:%S")
 
+
+# ---- Command-line argument handling ----
+if len(sys.argv) != 2:
+    print(f"Usage: python {sys.argv[0]} <input_csv>")
+    sys.exit(1)
+
+csv_filename = sys.argv[1]
+# ---------------------------------------
+
+
 sensors = {
     "hub": [],
     "worker1": [],
     "worker2": []
 }
 
-with open("thingspeak_feeds_2026-01-26T19-20-52.291Z.csv", newline="", encoding="utf-8") as file:
+with open(csv_filename, newline="", encoding="utf-8") as file:
     reader = csv.DictReader(file)
-    i = 0
     for row in reader:
         if not row["Device (field1)"]:
             continue
-        
+
         if "hub" in row["Device (field1)"]:
             sensors["hub"].append(row)
         if "@w1r" in row["Device (field1)"]:
             sensors["worker1"].append(row)
-        if "@w2r" in row["Device (field1)"]: 
+        if "@w2r" in row["Device (field1)"]:
             sensors["worker2"].append(row)
 
 
 total_gaps = 0
 for sensor in sensors.keys():
     lastTime = datetime.min
-    thisTime = datetime.min
-    first = True  # used to skip first row of each sensor
+    first = True  # skip first row of each sensor
+
     for row in sensors[sensor]:
         thisTime = parse_timestamp(row["Created (Chicago)"])
         delta = thisTime - lastTime
         lastTime = thisTime
-        if first == True:
+
+        if first:
             first = False
             continue
+
         if delta.total_seconds() > 3900:  # 65 minutes
-            print("Gap detected at row entry", row["Entry ID"], "with a gap of", delta)
+            print(
+                "Gap detected at row entry",
+                row["Entry ID"],
+                "with a gap of",
+                delta
+            )
             total_gaps += 1
 
 
 print("Scan complete. Detected", total_gaps, "gaps.")
+
+# Debug printing
+# for sensor in sensors.keys():
+#     print(sensor)
+#     for row in sensors[sensor]:
+#         print(row)
+#     print("=" * 126)
